7f7a6f340d913e0444964db7a8062ed52371ba1e
Yayi Abiodun <58443752+YAYI68@users.noreply.github.com>
2022-10-24 09:57:48 +0100

    commit

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -17,10 +17,8 @@ export const Carousel = ({sliderBlogs}) => {
         clearInterval(timer)
       }
     }, [curSlide])
-
-    // const images = ["./images/house1.jpg","./images/house2.jpg","./images/house3.jpg"]
-    let maxSlide = 3
-    // console.log({maxSlide})
+    let maxSlide = sliderBlogs.length
+    console.log({maxSlide})
 
     const moveRight = ()=>{
         if(curSlide >= maxSlide - 1){
@@ -45,7 +43,7 @@ export const Carousel = ({sliderBlogs}) => {
   
 
   return (
-     <div className='w-full h-[55rem]  mt-[2rem]'>
+     <div className='w-full h-[60rem]  mt-[2rem]'>
         <div  className='slider h-full w-full relative overflow-hidden rounded'>
         <button  onClick={moveLeft} className={`absolute left-[1%] top-[50%] text-[2.5rem] z-10 translate-y-[-50%] h-[4rem] w-[4rem] rounded-full bg-[#d5d1d1] flex items-center justify-center`}>
         <svg className="w-8 h-8 " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 16l-4-4m0 0l4-4m-4 4h18"></path></svg>
@@ -55,21 +53,21 @@ export const Carousel = ({sliderBlogs}) => {
         </button>
             {sliderBlogs.map((blog,i)=>(
                 <div   style={{
-                  transform: `translateX(${100 * (i-curSlide)})`
+                  transform: `translateX( ${100 * (i-curSlide)}% )`
                 }} className={`absolute top-[0] w-full  h-full   bg-no-repeat bg-cover   transition-transform `}> 
-                <img src={blog.imageURL} alt="" className=" h-full w-full brightness-[.5] object-top" />
+                <img src={blog.imageURL} alt="" className=" h-full w-full brightness-[.4] object-top" />
                 <div className='flex flex-col h-[70%] absolute top-[25%] left-[15%] w-[50%]'>
-                    <p className='text-[white] text-[1rem] '>July 13, 2022 <span className='mx-[.5rem]'>|</span>Admin</p>
-                    <h3 className='text-[3rem] text-[#ff8400] w-[70%]'>{blog.title}</h3>
-                    <p className='text-[white] text-[1.5rem]'> The content of my blog and story from london  The content of my blog and story from london The content of my blog and story from london  </p>
-                    <Link to="" className='text-[white] text-[1.2rem] px-[2rem] py-[1rem] bg-[#8034eb] w-fit text-center my-[1.5rem] rounded' >Read More</Link>
+                    <p className='text-[white] text-[1.5rem] '>July 13, 2022 <span className='mx-[.5rem]'>|</span>Admin</p>
+                    <h3 className='text-[3.5rem] text-[#ff8400] w-[70%]'>{blog.title}</h3>
+                    <p className='text-[white] text-[2.5rem]'> The content of my blog and story from london  The content of my blog and story from london The content of my blog and story from london  </p>
+                    <Link to={`/${blog.slug}`} state={{id:blog.id}} className='text-[white] text-[1.5rem] px-[2rem] py-[1rem] bg-[#8034eb] w-fit text-center my-[1.5rem] rounded' >Read More</Link>
                 </div>
               </div>    
                          
             ))}
             <div className='absolute bottom-5 left-1/2'>
               {sliderBlogs.map((image,i)=>(
-                <button onClick={()=>SlideBtn(i)} className='h-4 w-4 rounded-full  bg-white m-2'></button>
+                <button onClick={()=>SlideBtn(i)} className={`h-4 w-4 rounded-full ${curSlide===i?'bg-gray-700 border-2 border-white':'border-2 border-white'} bg-white m-2`}></button>
               ))}
             </div>     
           
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React,{useState,useEffect,useRef} from 'react'
-import { FaFacebookF,FaTwitter,FaInstagram,FaLinkedinIn,FaUserCircle } from 'react-icons/fa';
+import { FaFacebookF,FaTwitter,FaInstagram,FaLinkedinIn,FaUserCircle, FaSearch } from 'react-icons/fa';
 import { Link,useNavigate } from 'react-router-dom';
 import { useSelector,useDispatch } from 'react-redux';
 import {  logOutUser } from '../store/users/actions';
@@ -39,13 +39,13 @@ export const Header = () => {
       }
 
   return (
-    <header className="header bg-white  flex items-center justify-center fixed w-full z-10 px-3 py-[1.5rem]">
+    <header className="header bg-white  flex items-center justify-center fixed w-full z-20 px-3 py-[1.5rem]">
         <nav className="h-[80%] w-[90%]  flex justify-between items-center  ">
-            <h3 className='text-[2rem] text-[#8034eb] font-[500] italic w-[10%]'>Blog</h3>
-            <form className='w-[30%] flex items-center  h-[50%] border-2 border-[#8034eb] rounded'>
+            <Link to="/" className='text-[2rem] text-[#8034eb] font-[500] italic w-[10%]'>Blog</Link>
+            <form className='w-[30%] flex items-center  h-[50%] border-2 border-[#8034eb] rounded-md'>
                 <input type="text" className='h-full w-[90%] focus:outline-none text-[1.3rem] p-2'/>
                 <button className='w-[10%] flex items-center justify-center'>
-                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                <FaSearch  className='fill-[#8034eb] w-6 h-6' />
                 </button>
             </form>
             <ul className='w-[15%] flex items-center justify-between list-none'>
@@ -54,20 +54,20 @@ export const Header = () => {
                 <li><Link to="/profile" className="text-[1.5rem] font-[400] hover:text-[#8034eb]">About me</Link></li> 
             </ul>
             <div className='w-[7%] h-[50%] flex items-center justify-between'>
-                <button className="h-full"><FaFacebookF  className='h-full text-[1rem] fill-[#8034eb]'/></button>
-                <button className="h-full"><FaTwitter className='h-full text-[1rem] fill-[#8034eb]' /></button>
-                <button className="h-full"><FaInstagram className='h-full text-[1rem] fill-[#8034eb]'/></button>
-                <button className="h-full"><FaLinkedinIn className='h-full text-[1rem] fill-[#8034eb]'/></button>
+                <button className="h-full"><FaFacebookF  className=' w-6 h-6 fill-[#8034eb]'/></button>
+                <button className="h-full"><FaTwitter className='w-6 h-6 fill-[#8034eb]' /></button>
+                <button className="h-full"><FaInstagram className='w-6 h-6 fill-[#8034eb]'/></button>
+                <button className="h-full"><FaLinkedinIn className='w-6 h-6 fill-[#8034eb]'/></button>
             </div>   
             <div className='w-[5%]'>
                <button>
-                <svg class="w-8 h-8 " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"></path></svg>
+                <svg className="w-8 h-8 " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"></path></svg>
                </button>
             </div>
           {userInfo &&   
             <div className='h-[50%] w-[7%] relative'>
                 <div className='h-[40%] w-[40%] rounded-full cursor-pointer' onClick={()=>setUserDisplay(!userDisplay)}>
-                    <img src="./images/default.jpg" className='w-full h-full rounded-full' alt="" />
+                    <img src={userInfo.photoURL?userInfo.photoURL:"./images/default.jpg"} className='w-full h-full rounded-full' alt="" />
                 </div>
                 {userDisplay && 
                 <div ref={dropRef}  onClick={()=>setUserDisplay(false)} className='absolute w-[15rem] top-[110%]  flex flex-col bg-white text-[#8034eb] right-[5%] rounded-[.5rem]   shadow-md '>
