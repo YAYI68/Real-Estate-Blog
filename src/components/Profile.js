04bedc3bec42ed632eb33243c266f86e3a416afd
Yayi Abiodun <58443752+YAYI68@users.noreply.github.com>
2022-10-31 22:45:57 +0100

    commit

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,9 +3,11 @@ import { Section } from '../components/Section';
 import { FaFacebookF,FaTwitter,FaInstagram,FaLinkedinIn, FaPhoneAlt,FaTimes} from 'react-icons/fa';
 import {MdEmail } from 'react-icons/md';
 import { useSelector,useDispatch } from 'react-redux';
+import { useStateContext } from '../context/ContextProvider';
+
 
 export const Profile = ({setEditProfile, user}) => {
-   
+  const { userInfo }  = useStateContext();
 
   return (
     <Section className={'h-full mt-[5rem] lg:mt-[5rem] flex flex-col items-center'}>
@@ -25,7 +27,9 @@ export const Profile = ({setEditProfile, user}) => {
                 <button onClick={()=>window.open(user.instagram,'_blank')} className="h-full"><FaInstagram className='h-full text-[1.8rem] fill-[#8034eb]'/></button>
                 <button onClick={()=>window.open(user.linkedIn,'_blank')} className="h-full"><FaLinkedinIn className='h-full text-[1.8rem] fill-[#8034eb]'/></button>
             </div>
+            {userInfo.uid &&   
            <button onClick={()=>setEditProfile(true)} className='bg-[#8034eb] mt-2 text-white text-[1.5rem] rounded-lg py-5 px-[1.5rem]'>Edit Profile</button>
+            }
           </div>    
          </div>
           <div className='basis-[65%]  bg-white lg:w-[70%] md:w-[90%]  dark:text-white dark:bg-gray-800  shadow-lg p-[2rem] rounded-[.5rem] '>
@@ -49,7 +53,7 @@ export const Profile = ({setEditProfile, user}) => {
                  </div>
                   <div className='flex items-center mb-[1rem]' >
                   <MdEmail className='text-[1.8rem] mr-3 fill-[#8034eb]' />
-                  <p className='text-[1.5rem]'>{user.email}</p>
+                    <p className='text-[1.5rem]'>{user.email}</p>
                   </div>
                 </div>
                </div>
